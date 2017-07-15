066218d2d95dfc3ad2a269515250bdcdf75a908b
Paul Seyfert <pseyfert@cern.ch>
2017-07-15 21:27:03 +0200

    attempt to improve

--- swim.py
+++ swim.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import datetime
+from datetime import datetime
 import matplotlib.pyplot as plt
 
 from fitparse import FitFile
@@ -20,12 +20,26 @@ for m in mm:
         continue
     h = None
     t = []
+    lastref = datetime.strptime("1970-01-01 00:00:00","%Y-%m-%d %H:%M:%S")
     if m.as_dict()['name'] != 'hr': continue
+
+    names = []
     for xx in x:
+        names.append(xx['name'])
+
+    performed_update = False
+    if 'timestamp' in names:
+        # reference timestamp
+        if xx['name'] == 'timestamp':
+            lastref = datetime.strptime(xx['value'],"%Y-%m-%d %H:%M:%S")
+        performed_update = True
+    else:
         if xx['name'] == 'filtered_bpm':
             h = xx['value']
         if xx['name'] == 'event_timestamp':
             t.append(xx['value'])
+
+
     if type(h) is int:
         print x
         for xx in x:
@@ -37,19 +51,22 @@ for m in mm:
     if h is not None:
         try:
             if t[0] < ts[-1]:
-                pass
-                #hr = []
-                #ts = []
+                if performed_update:
+                    pass
+                else:
+                    print "PANIC"
         except:
             pass
+        performed_update = False
+
         for hh in xrange(len(h)):
             hr.append(h[hh])
-            ts.append(t[hh])
+            ts.append(lastref + t[hh])
 
 import matplotlib
 
-for h in hr:
-    print h
+#for h in hr:
+#    print h
 
 print "got HR values " + str(len(hr))
 print "got timestamps" + str(len(ts))
