432a90a78944bb98a92cae9db92d888d8c882598
Paul Seyfert <pseyfert@cern.ch>
2017-07-20 17:15:53 +0200

    attempt to tweak

--- swim.py
+++ swim.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from datetime import datetime
+from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 
 from fitparse import FitFile
@@ -13,30 +13,39 @@ mm = ff.get_messages()
 
 ts = []
 hr = []
+lastref = datetime.strptime("1970-01-01 00:00:00","%Y-%m-%d %H:%M:%S")
+performed_update = False
+
+entrycounter = 0
 for m in mm:
     try:
         x = m.as_dict()['fields']
     except:
         continue
+    entrycounter += 1
     h = None
     t = []
-    lastref = datetime.strptime("1970-01-01 00:00:00","%Y-%m-%d %H:%M:%S")
     if m.as_dict()['name'] != 'hr': continue
 
     names = []
     for xx in x:
         names.append(xx['name'])
 
-    performed_update = False
     if 'timestamp' in names:
+      for xx in x:
         # reference timestamp
         if xx['name'] == 'timestamp':
-            lastref = datetime.strptime(xx['value'],"%Y-%m-%d %H:%M:%S")
+            #print "found timestamp"
+            #lastref = datetime.strptime(xx['value'],"%Y-%m-%d %H:%M:%S")
+            lastref = xx['value']
         performed_update = True
     else:
+      for xx in x:
         if xx['name'] == 'filtered_bpm':
+            #print "found filtered bpm"
             h = xx['value']
         if xx['name'] == 'event_timestamp':
+            #print "found event timestamp"
             t.append(xx['value'])
 
 
@@ -52,7 +61,7 @@ for m in mm:
         try:
             if t[0] < ts[-1]:
                 if performed_update:
-                    pass
+                    print "let's do the time warp again"
                 else:
                     print "PANIC"
         except:
@@ -61,13 +70,17 @@ for m in mm:
 
         for hh in xrange(len(h)):
             hr.append(h[hh])
-            ts.append(lastref + t[hh])
+            ts.append(lastref + timedelta(seconds=t[hh]))
+            #ts.append(t[hh])
 
 import matplotlib
 
+#for t in ts:
+#    print t
 #for h in hr:
 #    print h
 
+print "analyszed " + str(entrycounter) + " entries"
 print "got HR values " + str(len(hr))
 print "got timestamps" + str(len(ts))
 
