ca1006a978324db705b2981e6f785ca16540e8d4
Paul Seyfert <pseyfert@cern.ch>
2018-02-02 09:22:15 +0100

    some initial ski file readout

---
+++ ski.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+
+from fitparse import FitFile
+
+
+ff = FitFile(os.path.abspath(sys.argv[1]))
+
+mm = ff.get_messages()
+
+ts = []
+hs = []
+
+tvs = []
+vs = []
+ps = []
+
+lapmarkers_UP = []
+lapmarkers_RUN = []
+
+flip_DOWN = []
+flip_UP = []
+
+total_dist_up = 0.
+total_dist_down = 0.
+
+total_height_up = 0.
+total_height_down = 0.
+
+downwards = True
+lastheight = 10000.
+lastflipdist = 0.
+lastflipheight = None
+
+for m in mm:
+    try:
+        x = m.as_dict()['fields']
+    except:
+        continue
+    h = None
+    t = None
+    tv = None
+    v = None
+    cur_dist = 0.
+
+    if m.as_dict()['name'] == 'lap':
+
+        isup = False
+        isrun = False
+        for xx in x:
+            if xx['name'] == 'Segment':
+                if "RUN" == xx['value']:
+                    isrun = True
+                elif "UP" == xx['value']:
+                    isup = True
+                else:
+                    print("paul, it's ", xx['value'])
+            if xx['name'] == 'timestamp':
+                lapmarker = xx['value']
+        # if isup:
+        #     lapmarkers_UP.append(lapmarker)
+        # elif isrun:
+        #     lapmarkers_RUN.append(lapmarker)
+        # else:
+        #     print("didn't find segment")
+        continue
+
+    if m.as_dict()['name'] != 'record':
+        continue
+
+    for xx in x:
+        if xx['name'] == 'altitude':
+            h = xx['value']
+        if xx['name'] == 'speed':
+            v = xx['value']
+        if xx['name'] == 'timestamp':
+            t = xx['value']
+        if xx['name'] == 'distance':
+            cur_dist = xx['value']
+
+    if v is not None:
+        tv = t
+
+    if h is not None and t is not None:
+
+        if downwards:
+            vs.append(3.6*v)
+            try:
+                ps.append(60./(3.6*v))
+            except ZeroDivisionError:
+                ps.append(0.)
+            tvs.append(tv)
+
+        if lastflipheight is None:
+            lastflipheight = h
+
+        if downwards and h < lastheight:
+            lastheight = h
+        elif downwards and h < lastheight + 10.:
+            pass  # fluctuation
+        elif downwards:
+            downwards = False
+            lastheight = h
+            flip_UP.append(t)
+            total_dist_down += cur_dist - lastflipdist
+            total_height_down += lastflipheight - h
+            lastflipdist = cur_dist
+            lastflipheight = h
+            vs.append(0)
+            ps.append(0)
+            tvs.append(t)
+        elif h > lastheight:  # upwards
+            lastheight = h
+        elif h > lastheight - 4.:  # upwards
+            pass  # fluctuation
+        else:
+            vs.append(0)
+            ps.append(0)
+            tvs.append(t)
+            downwards = True
+            lastheight = h
+            flip_DOWN.append(t)
+            total_dist_up += cur_dist - lastflipdist
+            total_height_up += h - lastflipheight
+            lastflipdist = cur_dist
+            lastflipheight = h
+
+        hs.append(h)
+        ts.append(t)
+
+
+print("Distance going down: ", total_dist_down)
+print("Distance going up:   ", total_dist_up)
+print("")
+print("Height going down: ", total_height_down)
+print("Height going up:   ", total_height_up)
+print("")
+
+dates = matplotlib.dates.date2num(ts)
+
+maxheight = max(hs)
+minheight = min(hs)
+
+fig = plt.figure()
+gs = gridspec.GridSpec(2, 1, height_ratios=[2, 1])
+
+ax0 = plt.subplot(gs[0])
+ax0.plot(ts, hs)
+ax0.set_xlim([minheight, maxheight])
+ax0.set_xlabel("time")
+ax0.set_ylabel("altitude")
+for lapmarker in flip_UP:
+    ax0.plot([lapmarker, lapmarker], [minheight, maxheight], 'b')
+for lapmarker in flip_DOWN:
+    ax0.plot([lapmarker, lapmarker], [minheight, maxheight], 'r')
+plt.gcf().autofmt_xdate()
+
+ax1 = plt.subplot(gs[1], sharex=ax0)
+# ax1.plot(tvs, ps)
+# ax1.set_ylabel("pace [min/km]")
+ax1.plot(tvs, vs)
+ax1.set_ylabel("speed [km/h]")
+plt.setp(ax0.get_xticklabels(), visible=False)
+
+yticks = ax0.yaxis.get_major_ticks()
+yticks[-1].label1.set_visible(False)
+yticks[0].label1.set_visible(False)
+yticks = ax1.yaxis.get_major_ticks()
+yticks[-1].label1.set_visible(False)
+yticks[0].label1.set_visible(False)
+
+plt.subplots_adjust(hspace=.0)
+plt.show()
